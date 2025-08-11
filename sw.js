3e26a858b01ea475849929d612b0f7667ac09b72
kulchoro101-lgtm <kulchoro101@gmail.com>
2025-08-12 00:08:29 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,23 @@
 
-const CACHE = 'teplostil-cache-v5';
-const ASSETS = ['./','./index.html','./cart.html','./manifest.json','./assets/icon.png','./assets/ph.png','./data/products.json'];
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+const CACHE = 'teplostil-cache-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './cart.html',
+  './manifest.json',
+  './assets/icon.png',
+  './assets/ph.png',
+  './data/products.json'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
 self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
-    const copy = res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res;
-  }).catch(()=>caches.match('./index.html'))));
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, copy));
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
 });
