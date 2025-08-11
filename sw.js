cdf02573db6ee6c6630163aa50e239e5bc890e4c
kulchoro101-lgtm <kulchoro101@gmail.com>
2025-08-12 01:06:23 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-
 const CACHE = 'teplostil-cache-v7';
 const ASSETS = [
   './',
@@ -9,17 +8,15 @@ const ASSETS = [
   './assets/ph.png',
   './data/products.json'
 ];
-self.addEventListener('install', function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('fetch', function(e){
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then(function(r){
-      return r || fetch(e.request).then(function(res){
-        const copy = res.clone();
-        caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
-        return res;
-      }).catch(function(){ return caches.match('./index.html'); });
-    })
+    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, copy));
+      return res;
+    }).catch(()=>caches.match('./index.html')))
   );
 });
